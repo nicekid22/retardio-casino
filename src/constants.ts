d8320ad1f4a0924b712c9f970a663b4950643cdc
nicekid22 <info.producermarket@gmail.com>
2024-09-20 09:41:08 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -60,8 +60,8 @@ export const DEFAULT_POOL = POOLS[0]
 export const TOKEN_METADATA: (Partial<TokenMeta> & {mint: PublicKey})[] = [
   {
     mint: FAKE_TOKEN_MINT,
-    name: 'Retardio',
-    symbol: 'RETARDIO',
+    name: 'Demo',
+    symbol: 'DEMO',
     image: '/fakemoney.png',
     baseWager: 1e9,
     decimals: 9,
